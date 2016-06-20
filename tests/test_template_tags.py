86a2d81b30fcd88f9d3833848010f454e047b811
Marco Giusti <marco.giusti@posteo.de>
2016-06-20 11:56:36 +0200

    Fix test.

--- tests/test_template_tags.py
+++ tests/test_template_tags.py
@@ -1,26 +1,11 @@
 import unittest
 
-
 import django
-from django.template import (
-    Context,
-    Template
-)
+django.setup()
 from django.conf import settings
 settings.configure()
-django.setup()
+from django.template import Context, Template
 from django.test.utils import override_settings
-try:
-    from django.template.base import add_to_builtins
-except ImportError:
-    # django >= 1.9
-    def add_to_builtins(module):
-        from django.template.engine import Engine
-        template_engine = Engine.get_default()
-        template_engine.builtins.append(module)
-        template_engine.template_builtins = \
-            template_engine.get_template_builtins(template_engine.builtins)
-
 from django_nvd3.templatetags.nvd3_tags import load_chart, include_container
 
 
@@ -32,8 +17,8 @@ class NVD3TemplateTagsTestCase(unittest.TestCase):
         return Template(string).render(context)
 
     def testPiechart(self):
-        xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries", "Blueberries",
-                 "Dates", "Grapefruit", "Kiwi", "Lemon"]
+        xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries",
+                 "Blueberries", "Dates", "Grapefruit", "Kiwi", "Lemon"]
         ydata = [52, 48, 160, 94, 75, 71, 490, 82, 46, 17]
         chartdata = {'x': xdata, 'y': ydata}
         charttype = "pieChart"
@@ -44,9 +29,8 @@ class NVD3TemplateTagsTestCase(unittest.TestCase):
 
     @override_settings(STATIC_URL='/static/')
     def test_include_chart_jscss_tag_custom_dirs(self):
-        add_to_builtins('django_nvd3.templatetags.nvd3_tags')
-
         rendered = self.render_template(
+            "{% load nvd3_tags %}"
             "{% include_chart_jscss css_dir='css' js_dir='js' %}"
         )
 
