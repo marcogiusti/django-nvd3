648378a800c0960f5632c47a48162a6b32152ae6
Marco Giusti <marco.giusti@posteo.de>
2016-06-20 20:34:19 +0200

    Refactor the tests.

--- tests.py
+++ tests.py
@@ -9,12 +9,37 @@ from django.test.utils import override_settings
 from django_nvd3.templatetags.nvd3_tags import load_chart, include_container
 
 
-class NVD3TemplateTagsTestCase(unittest.TestCase):
+HEADER = """\
+<link media="all" href="{css}" type="text/css" rel="stylesheet" />
+<script src="{d3js}" type="text/javascript" charset="utf-8"></script>
+<script src="{nvd3js}" type="text/javascript" charset="utf-8"></script>
 
-    def render_template(self, string, context=None):
-        context = context or {}
-        context = Context(context)
-        return Template(string).render(context)
+"""
+
+
+def render_template(string, context=None):
+    context = context or {}
+    context = Context(context)
+    return Template(string).render(context)
+
+
+@override_settings(STATIC_URL='/static/')
+class TestIncludeTag(unittest.TestCase):
+    # Not only does this test the output of the template tag,
+    # but also the order of including the static files - mainly
+    # d3 and nvd3 js files which should be in that order.
+
+    def test_custom_dirs(self):
+        rendered = render_template(
+            "{% load nvd3_tags %}"
+            "{% include_chart_jscss css_dir='css' js_dir='js' %}"
+        )
+        expected = HEADER.format(css="/static/css/nv.d3.min.css",
+                                 d3js="/static/js/d3.min.js",
+                                 nvd3js="/static/js/nv.d3.min.js")
+        self.assertEqual(rendered, expected)
+
+class NVD3TemplateTagsTestCase(unittest.TestCase):
 
     def testPiechart(self):
         xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries",
@@ -26,26 +51,6 @@ class NVD3TemplateTagsTestCase(unittest.TestCase):
 
         self.assertTrue(load_chart(charttype, chartdata, 'container', extra))
         self.assertTrue(include_container('container', height=400, width=600))
-
-    @override_settings(STATIC_URL='/static/')
-    def test_include_chart_jscss_tag_custom_dirs(self):
-        rendered = self.render_template(
-            "{% load nvd3_tags %}"
-            "{% include_chart_jscss css_dir='css' js_dir='js' %}"
-        )
-
-        # Not only does this test the output of the template tag,
-        # but also the order of including the static files - mainly
-        # d3 and nvd3 js files which should be in that order.
-        expected = (
-            '<link media="all" href="/static/css/nv.d3.min.css" '
-            'type="text/css" rel="stylesheet" />\n'
-            '<script src="/static/js/d3.min.js" '
-            'type="text/javascript" charset="utf-8"></script>\n'
-            '<script src="/static/js/nv.d3.min.js" '
-            'type="text/javascript" charset="utf-8"></script>\n\n'
-        )
-        self.assertEqual(rendered, expected)
 
 
 if __name__ == '__main__':
